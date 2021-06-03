57c0bc901aca745a03161c2846cc74018ecbd256
CONDUCTOR\giancarlo.castro <giancarlo.castro@conductor.com.br>
2021-06-03 16:18:30 -0300

    Java + Angular Parte 1

--- bootcamp/src/main/java/com/project/bootcamp/controller/StockController.java
+++ bootcamp/src/main/java/com/project/bootcamp/controller/StockController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,12 +16,12 @@ import java.util.List;
 public class StockController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<StockDTO> save(@RequestBody StockDTO dto){
+    public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO dto){
         return ResponseEntity.ok(dto);
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<StockDTO> update(@RequestBody StockDTO dto) {
+    public ResponseEntity<StockDTO> update(@Valid @RequestBody StockDTO dto) {
         return ResponseEntity.ok(dto);
     }
 
--- bootcamp/src/main/java/com/project/bootcamp/model/dto/StockDTO.java
+++ bootcamp/src/main/java/com/project/bootcamp/model/dto/StockDTO.java
@@ -1,13 +1,30 @@
 package com.project.bootcamp.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class StockDTO {
 
     private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
+    @DecimalMin(value="0.00")
+    @Digits(integer = 6, fraction = 2)
     private Double price;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate date;
+
+    @NotNull
+    @Digits(integer = 3, fraction = 2)
     private Double variation;
 
     public String getName() {
