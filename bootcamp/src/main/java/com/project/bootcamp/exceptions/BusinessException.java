1affab0eb13b6e6f81de08edbc0b52a33be9531a
CONDUCTOR\giancarlo.castro <giancarlo.castro@conductor.com.br>
2021-06-08 20:41:54 -0300

    Criando diretório separado para o Back-End

--- bootcamp/src/main/java/com/project/bootcamp/exceptions/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.project.bootcamp.exceptions;
-
-public class BusinessException extends RuntimeException{
-
-    public BusinessException(String message){
-        super(message);
-    }
-}
--- bootcamp/src/main/java/com/project/bootcamp/uitl/MessageUtils.java
+++
@@ -1,8 +0,0 @@
-package com.project.bootcamp.uitl;
-
-public abstract class MessageUtils {
-
-    public static final String STOCK_AlREADY_EXISTS = "Stock Already exists in the database";
-    public static final String NO_RECORDS_FOUND = "No records found.";
-
-}
